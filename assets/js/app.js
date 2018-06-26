ae5cf01f6e6c615c40e5868e100ff965aa012ff7
cha07 <charlotte.t@codeur.online>
2018-06-26 14:55:17 +0200

    geoloc

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+// geolocalisation
+
+function currentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((function (position) {
+            var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+            marker.bindPopup("Ma position :<br> Latitude : " + position.coords.latitude + ',<br>Longitude ' + position.coords.longitude).openPopup();
+        }));
+    } else {
+        alert("La géolocalisation n'est pas supportée par ce navigateur.");
+    }
+}
+
+// definir point de depart et choix de carte via leaflet
+var map = L.map('map', {
+    center: ['37.629562', '-116.849556'],
+    zoom: 16
+});
+
+L.tileLayer('https://api.mapbox.com/styles/v1/trelanor/cjivfv7xf4txn2qs4t8kaj9nb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHJlbGFub3IiLCJhIjoiY2ppdmVkd3QwMWFraTNxbXZleWV0ejF6cyJ9.RrESCs90t5bLD25_aI-DWA', {
+   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+   maxZoom: 18,
+   minZoom: 2,
+}).addTo(map);
+
+$('#myLocation').click(function (e) {
+    currentLocation();
+});
+
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+	<html lang="fr">
+		<head>
+			<meta charset="utf-8">
+			<meta name="robots" content="ISS, location, tracker"/>
+			<meta name="Description" content="ISS current location">
+			<meta http-equiv="X-UA-Compatible" content="ie=edge">
+			<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+			
+			
+			<title>Projet ISS</title>
+			
+			<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css"
+				integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+				crossorigin=""/>
+			<link rel="stylesheet" href="assets/css/style.css">
+		</head>
+		
+		
+		<body>
+			 <div id="map"></div>
+
+             <div>
+            <button id="myLocation" class="mylocation">Ma position</button>
+            </div>
+
+            
+			 
+			
+			<script
+			  src="https://code.jquery.com/jquery-3.3.1.min.js"
+			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+			  crossorigin="anonymous"></script>
+			<script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
+				integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
+				crossorigin=""></script>
+			<script src="assets/js/app.js"></script>
+		</body>
+	</html>
