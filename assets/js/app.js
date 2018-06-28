ced1f8b5115b3d1bf1ad09314c43eed06c2674f1
PierreHermey <pierre.hermey@laposte.net>
2018-06-28 08:54:41 +0200

    merge

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,22 +7,26 @@ var map = L.map('map', {
 });
 
 L.tileLayer('https://api.mapbox.com/styles/v1/trelanor/cjivfv7xf4txn2qs4t8kaj9nb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHJlbGFub3IiLCJhIjoiY2ppdmVkd3QwMWFraTNxbXZleWV0ejF6cyJ9.RrESCs90t5bLD25_aI-DWA', {
-   attribution: '&copy; <a href="https://www.mapbox.com/copyright">Mapbox</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-   maxZoom: 18,
-   minZoom: 2,
+    attribution: '&copy; <a href="https://www.mapbox.com/copyright">Mapbox</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 18,
+    minZoom: 2,
 }).addTo(map);
 
 $(function() {
-    $('#myLocation').click(function (e) {
+    $('#myLocation').click(function () {
         console.log(navigator.geolocation);
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
             function (position) {
-                var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
-                marker.bindPopup("Ma position :<br> Latitude : " + position.coords.latitude + ',<br>Longitude ' + position.coords.longitude).openPopup();
+
+                var geoLatitude = position.coords.latitude;
+                var geoLongitude = position.coords.longitude;
+
+                var marker = L.marker([geoLatitude, geoLongitude]).addTo(map);
+                marker.bindPopup("Ma position :<br> Latitude : " + geoLatitude + ',<br>Longitude ' + geoLongitude).openPopup();
                 
 
-                var requestURL = 'http://api.open-notify.org/iss-pass.json?lat='+ position.coords.latitude +'&lon='+ position.coords.longitude;
+                var requestURL = 'http://api.open-notify.org/iss-pass.json?lat='+ geoLatitude +'&lon='+ geoLongitude;
                 var request = new XMLHttpRequest();
                     request.open('GET', requestURL, true);
                     request.responseType = 'json';
