8b4ca88b81b5367ba118d144abfbcb245b5a62c4
cha07 <charlotte.t@codeur.online>
2018-06-28 09:01:42 +0200

    com

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+
+
+// definir point de depart ouverture page et choix de carte via leaflet
+var map = L.map('map', {
+    center: ['10', '10'],
+    zoom: 4
+});
+
+L.tileLayer('https://api.mapbox.com/styles/v1/trelanor/cjivfv7xf4txn2qs4t8kaj9nb/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidHJlbGFub3IiLCJhIjoiY2ppdmVkd3QwMWFraTNxbXZleWV0ejF6cyJ9.RrESCs90t5bLD25_aI-DWA', {
+   attribution: '&copy; <a href="https://www.mapbox.com/copyright">Mapbox</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+   maxZoom: 18,
+   minZoom: 2,
+}).addTo(map);
+
+$(function() {
+    $('#myLocation').click(function (e) {
+        console.log(navigator.geolocation);
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+            function (position) {
+                var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+                marker.bindPopup("Ma position :<br> Latitude : " + position.coords.latitude + ',<br>Longitude ' + position.coords.longitude).openPopup();
+            }, 
+            function(error) {
+                console.log(error);
+            }, {timeout:5000});
+        } else {
+            alert("La géolocalisation n'est pas supportée par ce navigateur.");
+        }
+        function getPoints($adresse) {
+            $api = simplexml_load_file("http://maps.googleapis.com/maps/api/geocode/xml?address=" . urlencode($adresse)."&sensor=true");
+                    if ($api=> status == "OK")
+                        return $api=>result=>geometry=>location=>lat . ',' . $api=>result=>geometry=>location=>lng;
+                    else {'false'}
+                        
+                }
+    });
+    
+})
+
+
+
---
+++ assets/js/iss.js
@@ -0,0 +1,60 @@
+function getValue(){
+var requestURL = 'http://api.open-notify.org/iss-now.json';
+var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        var ISS_Position = request.response.iss_position;
+            functionPosition(ISS_Position);
+
+        var ISS_Timestamp = request.response;
+            functionTimestamp(ISS_Timestamp);
+    }
+
+    function functionPosition(jsonObj) {
+        console.log(jsonObj['latitude']);
+        console.log(jsonObj['longitude']);
+
+        var ISSicon = L.icon({
+            iconUrl: 'assets/img/ISS-sm.png',
+        
+            iconSize:     [40, 40], // size of the icon
+            popupAnchor:  [-3, -26] // point from which the popup should open relative to the iconAnchor
+        });
+
+        var lastPosition = L.icon({
+            iconUrl: 'assets/img/passage-iss.png',
+        
+            iconSize:     [5, 5], // size of the icon
+        });
+    
+
+    var lastPositionISS = L.marker([jsonObj['latitude'],jsonObj['longitude']],{icon: lastPosition}).addTo(map);
+
+    var positionISS = L.marker([jsonObj['latitude'],jsonObj['longitude']],{icon: ISSicon}, {draggable: true}).addTo(map);
+    positionISS.bindPopup("ISS Position :<br> Latitude : " + jsonObj['latitude'] + ',<br>Longitude ' + jsonObj['longitude']).openPopup();
+
+    setTimeout(function(){ 
+        map.removeLayer(positionISS);
+    }, 5000);
+    
+    };
+
+    function functionTimestamp(jsonObj){
+        console.log(jsonObj['timestamp']);
+    }
+
+};
+    window.setInterval("getValue()", "5000", );
+
+	
+    
+    
+    
+    
+    
+    $.getJSON('http://api.open-notify.org/astros.json', function(astro) {
+	    console.log(astro);
+	});
